513524f46bc754d34a2ac833d595b6fd95db4e67
VytautasV <VytautasV@users.noreply.github.com>
2018-04-12 22:12:51 +0300

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,11 @@
+# scraper.py
+"""Ibreviary scraper.
+
+Scrapes ibreviary from www.hostname.com"""
+
+# In each commented line you write one step of your program.
+# create an ibreviariary settings url varible
+# go to settings page
+# ...
+# ...
+#... later you will add code below each line.
